c07f69007ef6a937e8c5ea96c56df775c6ed0a85
Jaxson Sprinkles <jaxstar24@gmail.com>
2025-12-12 15:46:23 +0000

    Change title to title_english

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import textwrap
+
+def wrap_text(text):
+    return '<br>'.join(textwrap.wrap(text, width=40))
 
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data.csv").dropna(subset=['title_english', 'synopsis'])
+df['synopsis'] = df['synopsis'].apply(lambda x: wrap_text(x))
 
-fig = px.scatter(df, x="x", y="y", hover_name="title", hover_data={"title": True, "x": False, "y": False})
+fig = px.scatter(df, x="x", y="y", hover_name="title_english", hover_data={"title_english": True, "x": False, "y": False})
 fig.update_traces(
-    hovertemplate=df["title"]
+    hovertemplate="<b>%{hovertext}</b><br>%{customdata[0]}<extra></extra>",
+    customdata=df[['synopsis']]
 )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, width="stretch")
