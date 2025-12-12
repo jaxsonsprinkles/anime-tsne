60ce4e9dd1369beb83ccef9e9d12df5ab5b01410
Jaxson Sprinkles <jaxstar24@gmail.com>
2025-12-12 02:05:36 +0000

    Update plotting with title tooltip

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import plotly.express as px
 
 df = pd.read_csv("data.csv")
 
-fig = px.scatter(df, x="x", y="y", hover_name="x", hover_data={"x": True, "y": False})
+fig = px.scatter(df, x="x", y="y", hover_name="title", hover_data={"title": True, "x": False, "y": False})
 fig.update_traces(
-    hovertemplate=df["x"]
+    hovertemplate=df["title"]
 )
 
 st.plotly_chart(fig, use_container_width=True)
